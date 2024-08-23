2ed33d36216c49fa4897eb5d40e54cf00f778835
Terry-mcgit <13884967603@163.com>
2024-08-23 14:33:47 +0800

    Complete the cheat detecter

--- common/src/main/java/fun/moystudio/mite_ctm/mixin/ServerPlayerMixin.java
+++ common/src/main/java/fun/moystudio/mite_ctm/mixin/ServerPlayerMixin.java
@@ -1,8 +1,8 @@
 package fun.moystudio.mite_ctm.mixin;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.client.telemetry.TelemetryProperty;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.entity.player.Player;
@@ -21,15 +21,20 @@ public abstract class ServerPlayerMixin extends Player {
 
     @Shadow public abstract boolean setGameMode(GameType arg);
 
+    @Shadow public abstract boolean isSpectator();
+
+    @Shadow public abstract void sendSystemMessage(Component arg);
+
+    @Shadow @Final public ServerPlayerGameMode gameMode;
+
     public ServerPlayerMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile) {
         super(level, blockPos, f, gameProfile);
     }
-    //写一下pls
-    //等等ok那我写
 
     @Inject(method = "tick", at = @At("TAIL"))
-    public void tick(CallbackInfo ci){
-        if(this.isCreative()){
+    public void tickMixin(CallbackInfo ci){
+        if(this.isCreative()||this.isSpectator()) {
+            this.sendSystemMessage(Component.nullToEmpty("§4Player <"+this.getGameProfile().getName()+"> tried to change its gamemode to \'"+this.gameMode.toString()+"\'!\nMITE-CTM just change its gamemode back to survival!"));
             this.setGameMode(GameType.SURVIVAL);
         }
     }
