0604338e17f42f0e73b2fa599065d09cf4978411
Terry-mcgit <13884967603@163.com>
2024-08-24 14:30:22 +0800

    Added BlockItem for blocks

--- common/src/main/java/fun/moystudio/mite_ctm/block/ModBlock.java
+++ common/src/main/java/fun/moystudio/mite_ctm/block/ModBlock.java
@@ -16,10 +16,6 @@ import java.util.function.Supplier;
 public class ModBlock {
     public static final DeferredRegister<Block> BLOCKS=DeferredRegister.create(MITE_CTM.MOD_ID, Registries.BLOCK);
     public static final DeferredRegister<Item> ITEMS=DeferredRegister.create(MITE_CTM.MOD_ID, Registries.ITEM);
-    public static final RegistrySupplier<Block> MITHRIL_BLOCK=registerBlock("mithril_block",()->
-            new IngotBlock(5.0F, SoundType.STONE));
-    public static final RegistrySupplier<Block> MITHRIL_ORE=registerBlock("mithril_ore",()->
-            new OreBlock(3.0F,SoundType.STONE,2,4));
     private static <T extends Block>RegistrySupplier<T> registerBlock(String name, Supplier<T> block){
         RegistrySupplier<T> toReturn = BLOCKS.register(name,block);
         registryBlockItem(name,toReturn);
@@ -28,6 +24,10 @@ public class ModBlock {
     private static <T extends Block>RegistrySupplier<Item> registryBlockItem(String name,RegistrySupplier<T> block){
         return ModItem.ITEMS.register(name,()->new BlockItem(block.get(),new Item.Properties()));
     }
+    public static final RegistrySupplier<Block> MITHRIL_BLOCK=registerBlock("mithril_block",()->
+            new IngotBlock(5.0F, SoundType.STONE));
+    public static final RegistrySupplier<Block> MITHRIL_ORE=registerBlock("mithril_ore",()->
+            new OreBlock(3.0F,SoundType.STONE,2,4));
     public static void register(){
         BLOCKS.register();
     }
